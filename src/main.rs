866e925c16103bcc843de1923d089d5797c8c6cb
Johannes Zeller <jo.zeller@protonmail.com>
2024-02-01 19:40:46 +0100

    create Rocket demo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::http::Status;
+use rocket::response::status;
+use rocket::serde::json::{Json, Value};
+use rocket::serde::json::serde_json::json;
+use rocket::serde::Serialize;
+
+#[derive(Serialize)]
+struct SumResult {
+    result: i32,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/ping")]
+fn ping() -> Value {
+    json!({
+        "message": "pong".to_string(),
+    })
+}
+
+#[get("/plus?<a>&<b>")]
+fn plus(a: Option<i32>, b: Option<i32>) -> Result<Json<SumResult>, status::Custom<String>> {
+    match (a, b) {
+        (Some(a_val), Some(b_val)) => {
+            Ok(Json(SumResult { result: a_val + b_val }))
+        },
+        _ => Err(status::Custom(Status::BadRequest, "invalid parameter".to_string())),
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+        .mount("/api", routes![ping, plus])
 }
